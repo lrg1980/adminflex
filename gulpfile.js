9372d33ac2f5271a78ea22577e710959aa495afa
lrg1980 <admin@binweb.net>
2019-04-15 10:48:32 -0300

    Armado de gulpfile y package.json functionando

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
-const gulp = require('gulp'),
-      sass = require('gulp-sass'),
-      autoprefixer = require('gulp-autoprefixer');
+const { watch, series, src, dest } = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
 
+function sassAndAuto () {
+  return src('scss/app.scss')
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(sass({
+      includePaths: ['scss']
+    }))
+    .pipe(dest('css'))
+}
 
-gulp.task('sass', () => {
-    gulp.src('scss/app.scss')
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }) )
-        .pipe(sass({
-            includePaths: ['scss']
-        }))
-        .pipe(gulp.dest('css'));
-});
-
-
-gulp.task('watch', ['sass'], () => {
-    gulp.watch(["scss/*.scss"], ['sass']);
-});
+// eslint-disable-next-line semi
+exports.sassAndAuto = sassAndAuto;
+// eslint-disable-next-line semi
+exports.default = series(sassAndAuto);
+// eslint-disable-next-line semi
+watch(['scss/*.scss'], series(sassAndAuto));
